68193033544dd082190a289d7ba25431238437e7
napatswift <napat.do@ku.th>
2022-04-08 16:31:57 +0700

    Using array to contain columns
    - using vector to contain string of each column
    - styling code

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 PDF_FILE = ./vote-log/20190823113634A11.pdf[0]
-main_built := ./built/main.o
+main_built := ./main.o
 
 main: convert
-	g++ -O3 -std=c++11 ./src/main.cpp ./built/convert.o -o $(main_built) `pkg-config --cflags --libs tesseract opencv4 Magick++` 
+	g++ -O3 -std=c++11 ./src/main.cpp ./convert.o -o $(main_built) `pkg-config --cflags --libs tesseract opencv4 Magick++` 
 	$(main_built) $(PDF_FILE)
 
 convert:
-	g++ -O3 -std=c++11 -c ./src/convert.cpp `pkg-config --cflags --libs Magick++` -o ./built/convert.o
+	g++ -O3 -std=c++11 -c ./src/convert.cpp `pkg-config --cflags --libs Magick++` -o ./convert.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <tesseract/baseapi.h>
+#include <vector>
 #include <algorithm>
+
+#include <tesseract/baseapi.h>
+
 #include "convert.hpp"
 
-std::vector<std::vector<Rect>> lineBlock(Mat src){
+std::vector<std::vector<Rect>> lineBlock(Mat src) {
     std::vector<std::vector<Rect>> lineBounds;
 
-    if (src.empty()){
+    if (src.empty()) {
         std::cerr << "[error]: image is empty" << std::endl;
         return lineBounds;
     }
@@ -33,7 +36,7 @@ std::vector<std::vector<Rect>> lineBlock(Mat src){
     findContours(src, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
 
     std::vector<Rect> rects;
-    for (std::vector<Point> contour: contours){
+    for (std::vector<Point> contour: contours) {
         Rect rect = boundingRect(contour);
         rects.push_back(rect);
     }
@@ -41,23 +44,28 @@ std::vector<std::vector<Rect>> lineBlock(Mat src){
     int prevR = -1;
     bool isSpace = true;
     size_t lineNumber = 0;
-    for (size_t r = 0; r < src.rows; r++){
-        if (isSpace){
-            if (!hist(r)){
+    for (size_t r = 0; r < src.rows; r++) {
+        if (isSpace) {
+            if (!hist(r)) {
                 isSpace = false;
                 prevR = r;
             }
         } else {
-            if (hist(r)){
+            if (hist(r)) {
                 isSpace = true;
                 std::vector<Rect> line;
                 do {
                   Rect rect = rects.back();
                   double mid = rect.y + rect.height/2;
-                  if (!(mid < r && mid > prevR)) break;
+
+                  if (!(mid < r && mid > prevR)) {
+                    break;
+                  }
+
                   line.push_back(rect);
                   rects.pop_back();
                 } while (!rects.empty());
+
                 lineBounds.push_back(line);
             }
         }
@@ -65,12 +73,12 @@ std::vector<std::vector<Rect>> lineBlock(Mat src){
     return lineBounds;
 }
 
-bool rectComparator(Rect r1, Rect r2){
+bool rectComparator(Rect r1, Rect r2) {
   //comparing in ascending order
   return r1.x < r2.x;
 }
 
-int main(int n_args, char ** args){
+int main(int n_args, char ** args) {
   if (n_args < 2) {
     std::cout << "Usage: main filename" << std::endl;
     return EXIT_FAILURE;
@@ -78,7 +86,7 @@ int main(int n_args, char ** args){
   std::string filePath = args[1];
   
   size_t start = filePath.rfind('/');
-  if (start == std::string::npos){
+  if (start == std::string::npos) {
     start = 0;
   } else {
     start++;
@@ -86,36 +94,43 @@ int main(int n_args, char ** args){
   size_t end = filePath.rfind('.');
   size_t bracket = filePath.rfind('[');
   std::string extension;
-  if (bracket == std::string::npos){
+  if (bracket == std::string::npos) {
     extension = filePath.substr(end+1);
   } else {
     extension = filePath.substr(end+1, bracket-end-1);
   }
 
-  if (extension != "pdf"){
+  if (extension != "pdf") {
     std::cout << "wrong file extenstion \"." << extension << "\"" << std::endl;
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
   }
 
   std::string filename = filePath.substr(start, end-start);
   std::vector<Mat> images =  convert(filePath);
 
-  std::ofstream fileOut;
+  std::ofstream fileOut, textBoxes;
   fileOut.open(filename + ".csv");
-
+  textBoxes.open(filename + ".csv");
+  
   tesseract::TessBaseAPI *tessOcr = new tesseract::TessBaseAPI();
   tessOcr->Init("./tessdata/", "tha", tesseract::OEM_LSTM_ONLY);
   tessOcr->SetPageSegMode(tesseract::PSM_SINGLE_LINE);
-  for (size_t i = 0; i < images.size(); i++){
+
+  for (size_t i = 0; i < images.size(); i++) {
     std::vector<std::vector<Rect>> lines = lineBlock(images[i]);
     bool table = false;
-    std::vector<int> columnPositions;
     int prevCol = 0;
-    for (std::vector<Rect> rects: lines){
+    std::vector<int> columnPositions;
+    std::vector<std::string> row;
+    for (std::vector<Rect> rects: lines) {
       //sort the list in ascending order
       std::sort(rects.begin(), rects.end(), rectComparator);
-      std::string lineString;
-      for (Rect rect: rects){
+
+      //malloc column
+      if (table)
+        row.assign(columnPositions.size(), std::string());
+
+      for (Rect rect: rects) {
         Mat croped = images[i](rect);
         tessOcr->SetImage(croped.data, croped.cols, croped.rows, 3, croped.step);
         std::string text = std::string(tessOcr->GetUTF8Text());
@@ -123,38 +138,51 @@ int main(int n_args, char ** args){
         while(!text.empty() && text.back() == '\n') text.pop_back();
         
         //detect header
-        if (text == "ลําดับที่"){
+        if (!table && text == "ลําดับที่") {
           table = true;
           columnPositions.push_back(0);
-          for (size_t j = 1; j < rects.size(); j++){
+          for (size_t j = 1; j < rects.size(); j++) {
             columnPositions.push_back(rects[j].tl().x);
           }
           //skip the header
           break;
         }
         //skip the line if table did not start yet
-        if (!table){
+        if (!table) {
           break;
         }
+        //determine column
         int col = 0;
         for (size_t j = 0;
              j < columnPositions.size() && 
              rect.x+rect.width/2 > columnPositions[j];
-             ++j){
+             ++j) {
           col = j;
         }
-        if (col)
-          lineString.push_back(prevCol == col ? ' ' : ',');
-        prevCol = col;
-        lineString += text;
+
+        //feed a whitespace to non-empty string
+        if (!row[col].empty()) row[col].push_back(' ');
+        row[col].append(text);
       }
-      if (lineString.size()){
-        lineString.push_back('\n');
-        fileOut << lineString;
+
+      if (row.size()) {
+        for (size_t j = 0; j < row.size(); j++) {
+          fileOut << row[j];
+          
+          //put a comma
+          if (j+1<row.size()) fileOut.put(',');
+        }
+        //feed a new line
+        fileOut.put('\n');
       }
     }
   }
-  if (fileOut.is_open()) fileOut.close();
+
+  //close output file
+  if (fileOut.is_open())
+    fileOut.close();
+
   tessOcr->End();
+
   return EXIT_SUCCESS;
 }
