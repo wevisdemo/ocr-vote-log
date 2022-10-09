9c1ea749f878c8a85af576f36ba9924494c4de4e
napatswift <napat.do@ku.th>
2022-10-09 20:35:31 +0700

    Update downloader.py

--- src/downloader.py
+++ src/downloader.py
@@ -4,6 +4,7 @@ import requests
 import numpy as np
 import cv2
 import fitz
+from PIL import Image
 
 def download(url: str) -> str:
     filename = url.rsplit('/', maxsplit=1)[1]
@@ -25,24 +26,24 @@ def convert_image(file_path: str) -> np.ndarray:
     images: List = list()
     max_w, max_h = (0, 0)
     
-    zoom_mat = fitz.Matrix(3, 3)
+    zoom_mat = fitz.Matrix(3., 3.)
     for page in doc:
         pix_map = page.get_pixmap(matrix=zoom_mat)
         height, width = pix_map.height, pix_map.width
-        if max_h < height:
-            max_h = height
-        if max_w < width:
-            max_w = width
 
-        image_mat = np.frombuffer(pix_map.samples_mv, dtype=np.uint8).reshape((height, width, 3))
+        if max_h < height: max_h = height
+        if max_w < width: max_w = width
+
+        image = Image.frombytes('RGB', (width, height), pix_map.samples)
+        image_mat = np.array(image)
         images.append(image_mat)
-
+    
     padded_images = []
     for image_mat in images:
         # add padding
         height, width = image_mat.shape[:2]
         padded = cv2.copyMakeBorder(
-            image_mat, 0, max_h-height, 0, max_w-width, cv2.BORDER_WRAP)
+            image_mat, 0, max_h-height, 0, max_w-width, cv2.BORDER_CONSTANT, 255)
         padded_images.append(padded)
 
     # convert to single array
