1e4c8f6f0e2769b5258ec73aeec1b94d81dbc15f
napatswift <napat.do@ku.th>
2022-10-12 00:55:50 +0700

    Update recognizer.py

--- src/recognizer.py
+++ src/recognizer.py
@@ -9,12 +9,14 @@ from src.utils import onehot
 
 class Recognizer:
     def __init__(self, reader: easyocr.Reader, images: np.ndarray) -> None:
+        self.interested_chars = '- กขคฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฤเแโใไะาุูิีืึั่้๊๋็์ำํฺฯๆ0123456789'
         self.images = images
         self.detector = Detector(images)
         self.reader: easyocr.Reader = reader
         self.tab_detector = TableDetector()
 
     def get_text_image_list(self, image, text_bbox) -> List[np.ndarray]:
+
         text_images: List = list()
         for x, y, w, h in text_bbox:
             ti = image[y:y+h, x:x+w]
@@ -45,13 +47,14 @@ class Recognizer:
         return self.detector.filter(bbox) & (
             (x_pos > x0) & (x_pos < x1) & (y_pos > y0) & (y_pos < y1))
 
-    def get_text(self, image: np.ndarray):
-        M = 15  # margin
+    def get_text(self, im_idx: int):
+        assert isinstance(im_idx, int)
+        image: np.ndarray = self.images[im_idx]
 
         # find table region
         pil_img = Image.fromarray(image)
         tab = self.tab_detector(pil_img).tolist()
-        x0, y0, x1, y1 = [int(x) for x in tab]
+        del pil_img
 
         bbox: np.ndarray = self.detector.detect_text(image)
 
@@ -59,12 +62,13 @@ class Recognizer:
         bbox_list: List = filtered_bbox.tolist()
         bbox_list.sort(key=lambda x: x[0])
         filtered_bbox = np.array(bbox_list)
+        del bbox_list
 
         # draw y hist
         y_hist = np.zeros(image.shape[0])
         for x, y, w, h in filtered_bbox:
             y_hist[y:y+h] += w
-        y_hist = y_hist > y_hist.mean() - y_hist.std()
+        y_hist = y_hist != 0
 
         # find columns
         columns = self.get_column(image, filtered_bbox)
@@ -73,51 +77,65 @@ class Recognizer:
         gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         text_images = self.get_text_image_list(
             image=gray_image, text_bbox=filtered_bbox)
-        text_image_arr = np.array(text_images)
-
-        col_num = np.unique(columns)
-        text_list: List = list()
+        text_image_arr = np.array(text_images, dtype=object)
 
         included = np.zeros(filtered_bbox[:, 1].shape, dtype=bool)
 
         text_lines = []
         prev_y = 1
-        for i, y in enumerate(y_hist):
-            if y and not prev_y:
+
+        # DEV
+        draw_line_img = image.copy()
+        draw_line_img = self.__dev_draw_rect(draw_line_img, filtered_bbox)
+        ###
+
+        for ypos, ybool in enumerate(y_hist):
+            if not ybool and prev_y:
                 # find boxex that are in line
-                line_b = (filtered_bbox[:, 1] < i) & (~included)
+                line_b = (filtered_bbox[:, 1] < ypos) & (~included)
                 if line_b.sum() == 0:
                     continue
-                text_cols = columns[line_b]
-                text_imgs = text_image_arr[line_b]
-                line_text = self._text_line(text_imgs, text_cols)
+
+                draw_line_img = self.__dev_draw_y_hline(draw_line_img, ypos)
+
+                line_text = self._text_line(
+                    text_image_arr[line_b], columns[line_b])
+
                 text_lines.append(line_text)
-                included[line_b] = i
-            prev_y = y
+                included[line_b] = ypos
+            prev_y = ybool
+
+        cv2.imwrite('line_p{}.png'.format(im_idx), draw_line_img)
 
         return text_lines
+
+    def __dev_draw_rect(self, image, bbox):
+        for x, y, w, h in bbox:
+            image = cv2.rectangle(image, (x, y), (x+w, y+h), 255, 1)
+
+        return image
+
+    def __dev_draw_y_hline(self, image, ypos):
+        image = cv2.line(image, (0, ypos), (image.shape[1], ypos), 255, 1)
+        
+        return image
 
     def _text_line(self, text_images, text_cols):
         line_text = [list() for _ in range(int(text_cols.max())+1)]
         for img_txt, col_txt in zip(text_images, text_cols):
-            recog_output = self.reader.recognize(img_txt)
+            recog_output = self.reader.recognize(
+                img_txt, allowlist=list(self.interested_chars))
             text = recog_output[0][1]
             col_idx = int(col_txt)
             line_text[col_idx].append(text)
-        return line_text
 
-    def _line_order(self, bboxes: np.array, lines: np.ndarray) -> np.ndarray:
-        one_hot = onehot(lines)
-        vert_sum = one_hot.T@bboxes  # summation of each line
-        box_count = one_hot.sum(axis=0)  # number of boxes each line
-        box_mid = (vert_sum[:, 1])
-        avg = (box_mid/box_count)
-        return avg.argsort()
+        return line_text
 
     def recognize(self):
         text_list = []
-        for image in self.images:
-            text = self.get_text(image)
+        img_num = len(self.images)
+        for i in range(img_num):
+            text = self.get_text(i)
             text_list.append(text)
 
         return text_list
